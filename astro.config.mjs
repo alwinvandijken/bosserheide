5e79fb3fd90c25e358b35c028e9392ca674c9a99
Alwin van Dijken <alwin.vandijken@brightfox.eu>
2024-09-16 08:43:08 +0200

    Add English pages and implement i18n support
    Created English versions of the homepage and "about us" pages. Also added a Google Map component and configured i18n support for Dutch, German, and English locales.

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,4 +9,8 @@ export default defineConfig({
   site: 'https://bosserheide.nl/', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true, // Generate sitemap (set to "false" to disable)
   integrations: [sitemap(), mdx(), lit(), icon()], // Add renderers to the config
+  i18n: {
+    defaultLocale: "nl",
+    locales: ["de", "en","nl"],
+  }
 });
